3c9ca695c98ccb3a7b4a9507ac9d7d6977da30c9
Oliver Samson <oliver.samson@gmail.com>
2016-10-16 16:10:44 +0100

    Remove trace from combobox datasource

--- TextOn/TextOn.Studio/DefineNounsComboBoxDataSource.cs
+++ TextOn/TextOn.Studio/DefineNounsComboBoxDataSource.cs
@@ -15,28 +15,23 @@ namespace TextOn.Studio
 
         public override nint ItemCount (NSComboBox comboBox)
         {
-            Console.Error.WriteLine ("ComboBox ItemCount {0}", suggestions.Length);
             return suggestions.Length;
         }
 
         public override NSObject ObjectValueForItem (NSComboBox comboBox, nint index)
         {
-            Console.Error.WriteLine ("ObjectValueForItem {0} {1}", index, suggestions.Length);
             if (index >= suggestions.Length) return new NSString ("");
             return new NSString (suggestions [index]);
         }
 
         public override nint IndexOfItem (NSComboBox comboBox, string value)
         {
-            Console.Error.WriteLine ("ComboBox IndexOfItem {0}", value);
             int i = 0;
             foreach (var suggestion in suggestions) {
-                Console.Error.WriteLine ("ComboBox Item {0} is {1} looking for {2}", i, suggestion, value);
                 if (suggestion == value) return i;
                 ++i;
             }
             if (i > suggestions.Length) return -1;
-            Console.Error.WriteLine ("ComboBox IndexOfItem {0} Exit {1}", value, i);
             return i;
         }
 
