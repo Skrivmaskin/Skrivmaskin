c55b37dad11a151a24c62c4d70062dfaded81fb6
Skrivmaskin <jonas.kiessling@gmail.com>
2016-10-24 23:30:20 +0100

    word counting, sentence counting and character counting with tests

--- TextOn/StatsPlaying/BasicStats.fs
+++ TextOn/StatsPlaying/BasicStats.fs
@@ -7,6 +7,58 @@ open System
 module BasicStats = 
    
 (*
+	ToDo: Write the stats functions for the compiled tree instead
+		- improce sentence break logic...
+		- write some simple test
+*)
+ 
+ let private alphabetLower = "abcdefghijklmnopqrstuvwxyzåäö"
+ let private alphabetUpper = alphabetLower.ToUpper()
+ let private alphabetLowerSet = Set.ofArray <| alphabetLower.ToCharArray()
+ let private alphabetUpperSet = Set.ofArray <| alphabetUpper.ToCharArray()
+ let private letterSet = Set.union alphabetLowerSet alphabetUpperSet
+ let private isCharLetter (s : char) = letterSet.Contains(s)
+ let private whiteSpace = ' '
+ let private delimiters = ".,;:!?".ToCharArray() |> Set.ofArray
+
+ let private isWhitespace (c : char) = c = whiteSpace
+ let private isNotWhitespace (c : char) = not(isWhitespace c)
+
+ let private charArrayToString (c : char []) = c |> Array.fold (fun acc c -> acc+(c.ToString())) ""
+
+ let removeTrailingWhitespaceFromChars (a : char []) = 
+      let mutable n = a.Length
+      if n > 0
+      then
+       while n > 0 && a.[n - 1] |> isWhitespace do n <- n - 1
+       a.[0 .. (n - 1)]
+      else
+          [||]
+
+ let removeTrailingWhitespace (s : string) = 
+     s.ToCharArray() |> removeTrailingWhitespaceFromChars |> charArrayToString
+
+ let removeInitialWhitespace (s : string) = s.ToCharArray() |> Array.rev  |> removeTrailingWhitespaceFromChars |> Array.rev  |> charArrayToString
+
+
+
+ // a sentence end is marked by a letter character and a "."
+ let countSentenceBreaksInString (s : string) = 
+     let a = s.ToCharArray()
+     let rec f (s : char []) = 
+         if s.Length < 2 then 0
+         else (if s.[0] |> isCharLetter && s.[1] = '.' then 1 else 0) + f s.[1 .. ]
+     f a 
+
+
+ // returns number of characters in a string, including whitespace and non-letters
+ let countCharactersInString (s : string) = s.ToCharArray().Length
+
+ // a word is defined as a whitespace/start of string follwed by non-whitespace, followed by whitespace, or any of ".,:;!?" or end of string
+ let countWordsInString (s : string) = 
+     s.Split(' ') |> Array.map (fun a -> a |> removeTrailingWhitespace |> removeInitialWhitespace) |> Array.filter (fun x -> x.Length > 0) |> Array.length
+
+(*
 	ToDo: 	improve logic for word and letter counter
 			- wich letters should count?
 			- how do I detect a word?
@@ -16,6 +68,8 @@ module BasicStats =
 
 *)
 
+
+(*
  
  let private vanillaStat (choiceBehaviour : seq<float> -> float) (metric : TextNode -> float)  = 
     let rec f (n : INode) = 
@@ -56,3 +110,5 @@ module BasicStats =
            let s = s |> Seq.item (len / 2)
            s
     vanillaStat (median)
+
+*)
--- TextOn/StatsPlaying/Script.fsx
+++ TextOn/StatsPlaying/Script.fsx
@@ -10,6 +10,22 @@ open TextOn.Design
 open System
 
 
+let shouldBeThree = BasicStats.countSentenceBreaksInString "Hello, my name is Jonas. I am from Sweden. This is a silly text."
+let shouldBeZero = BasicStats.countSentenceBreaksInString ". I don't have a sentence"
+let shouldBeTwo = BasicStats.countSentenceBreaksInString ". I have a couple of sentences....... But not many."
+
+(* test whitespace cropping *)
+let _ = "test      " |> BasicStats.removeTrailingWhitespace = "test"
+let _ = "       test" |> BasicStats.removeInitialWhitespace = "test" 
+
+(* test number of words *)
+let zeroWords = "        " |> BasicStats.countWordsInString
+let oneWord = "   ddmv " |> BasicStats.countWordsInString
+let threeWords = " scd,ll'cds      cdsc' d" |> BasicStats.countWordsInString
+
+
+
+(*
 let textNode1 = TextOn.Design.TextNode("Hello, I", true) :> INode
 let textNode2 = TextOn.Design.TextNode("Hello, I am a", true) :> INode
 let textNode3 = TextOn.Design.TextNode("Hello, I am a text node", true) :> INode
@@ -36,3 +52,4 @@ let mi2 = BasicStats.minStat BasicStats.wordCounter sequenceNode
 let ma2 = BasicStats.maxStat BasicStats.wordCounter sequenceNode
 let av2 = BasicStats.averageStat BasicStats.wordCounter sequenceNode
 
+*)
